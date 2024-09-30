2acbc58e5e6e2a6270dc1e909ad7ab974293372e
suynnn <suy00065@gmail.com>
2024-09-30 18:13:05 +0900

    Feat: 0926 week2 day2 - BOJ9372 상근이의 여행.py

---
+++ week2/day5/BOJ9372 상근이의 여행.py
@@ -0,0 +1,11 @@
+from sys import stdin
+
+testcase = int(input())
+
+for _ in range(testcase):
+    n, m = map(int, stdin.readline().rstrip().split())
+
+    for _ in range(m):
+        a_b = stdin.readline()
+
+    print(n-1)
