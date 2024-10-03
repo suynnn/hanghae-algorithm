44e32bbe7a808181603a603aa668e1ea5ece9348
suynnn <suy00065@gmail.com>
2024-10-03 19:13:55 +0900

    Feat: 1003 week3 day2 - BOJ1026 보물.py

---
+++ week3/day2/BOJ1026 보물.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+a = sorted(list(map(int, input().split())))
+b = sorted(list(map(int, input().split())), reverse=True)
+
+ans = 0
+for i in range(n):
+    ans += a[i] * b[i]
+
+print(ans)
